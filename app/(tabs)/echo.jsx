7c64ec480b6da23740582435be311a35a1b8c2a9
Muskan <muskan2002219@gmail.com>
2025-06-21 16:11:15 +0530

    ui fixed

---
+++ app/(tabs)/echo.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const echo = () => {
+  return (
+    <View>
+      <Text>echo</Text>
+    </View>
+  )
+}
+
+export default echo
